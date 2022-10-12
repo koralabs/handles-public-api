0c71f16cdaf9adaf2da1906ca23c4f3370fa0d7e
Papa Goose <papa.goose@koralabs.io>
2022-10-12 15:25:46 -0700

    relative

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/swagger/public-api.yml",
+    url: "./public-api.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
