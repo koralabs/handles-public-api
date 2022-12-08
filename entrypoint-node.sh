b956ca76e1e01a36caa1d049d0c228ab7760becf
Papa Goose <papa.goose@koralabs.io>
2022-12-07 16:06:08 -0800

    DEFAULT_NODE_OPTIONS=--max-old-space-size=12288

--- entrypoint-node.sh
+++ entrypoint-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+DEFAULT_NODE_OPTIONS=--max-old-space-size=12288
 /root/cardano-node-ogmios.sh $@ &
-NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK:-mainnet} npm start &
+NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK:-mainnet} NODE_OPTIONS="${NODE_OPTIONS:-$DEFAULT_NODE_OPTIONS}" npm start &
 wait
--- entrypoint-ogmios.sh
+++ entrypoint-ogmios.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+DEFAULT_NODE_OPTIONS=--max-old-space-size=12288
 /bin/ogmios $@ &
-NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK:-mainnet} npm start &
+NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK:-mainnet} NODE_OPTIONS="${NODE_OPTIONS:-$DEFAULT_NODE_OPTIONS}" npm start &
 wait
