b8492e289db38f694e1c3756d1fc5c26df737730
Papa Goose <papa.goose@koralabs.io>
2022-12-11 21:37:52 -0800

    Simplifying docker images

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,17 @@ FROM ubuntu:22.04
 ENV NETWORK=mainnet
 WORKDIR /app
 SHELL ["/bin/bash", "-c"]
-ADD ./dist/ ./
-ADD entrypoint-ogmios.sh ./entrypoint.sh
+ADD ./dist/ entrypoint.sh ./
 RUN \
-    apt install -y && apt update -y && apt install -y git curl socat jq unzip && \
-    chmod +x ./entrypoint.sh && \
+    apt install -y && apt update -y && apt install -y git curl socat jq unzip tini && \
+    curl -fsSL https://update-cardano-mainnet.iohk.io/cardano-node-releases/cardano-node-1.35.4-linux.tar.gz | tar -xz && \
+    rm -f ./cardano-node-1.35.4-linux.tar.gz && \
+    chmod +x ./cardano-node && chmod +x ./entrypoint.sh && \
     git clone https://github.com/input-output-hk/cardano-world.git && \
     curl -sL https://github.com/CardanoSolutions/ogmios/releases/download/v5.5.7/ogmios-v5.5.7-x86_64-linux.zip -o ogmios.zip && \
     unzip ogmios.zip -d ./ogmios-install && rm ogmios.zip && \
     cp ./ogmios-install/bin/ogmios /bin/ogmios && chmod +x /bin/ogmios && rm -rf ./ogmios-install && \
     curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && apt install -y nodejs
-EXPOSE 3141 1337
+EXPOSE 3141
 STOPSIGNAL SIGINT
-HEALTHCHECK --interval=10s --timeout=5s --retries=1 CMD /bin/ogmios health-check
-ENTRYPOINT [ "/app/entrypoint.sh" ]
+ENTRYPOINT [ "tini", "-g", "--", "./entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DEFAULT_NODE_OPTIONS=--max-old-space-size=12288
+NETWORK=${NETWORK:-mainnet}
+MODE=${MODE:-both}
+
+if [[ "$@" != *"--host"* ]]
+then
+    HOST="--host 0.0.0.0"
+fi
+if [[ "$@" != *"--node-config"* ]]
+then
+    NODE_CONFIG="--node-config ./cardano-world/docs/environments/${NETWORK}/config.json"
+fi
+if [[ "$@" != *"--node-socket"* ]]
+then
+    NODE_SOCKET="--node-socket /ipc/node.socket"
+fi
+
+if [[ "${MODE}" == "ogmios" || "${MODE}" == "both" ]]; then
+    ogmios $HOST $NODE_CONFIG $NODE_SOCKET $@ &
+    ogmios_status=$?
+
+    if [ $ogmios_status -ne 0 ]; then
+    echo "Failed to start ogmios: $ogmios_status"
+    exit $ogmios_status
+    fi
+
+    NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK} NODE_OPTIONS="${NODE_OPTIONS:-$DEFAULT_NODE_OPTIONS}" npm run start:forever &
+fi
+
+if [[ "${MODE}" == "cardano-node" || "${MODE}" == "both" ]]; then
+    DB_FILE=/db/protocolMagicId
+    if [ "${NETWORK}" == "mainnet" ] && [ ! -f "$DB_FILE" ]; then
+        curl -o - https://downloads.csnapshots.io/snapshots/mainnet/$(curl -k -s https://downloads.csnapshots.io/snapshots/mainnet/mainnet-db-snapshot.json| jq -r .[].file_name ) | lz4 -c -d - | tar -x -C /
+    fi
+
+    ./cardano-node run +RTS -N -RTS \
+        --config ./cardano-world/docs/environments/${NETWORK}/config.json \
+        --topology ./cardano-world/docs/environments/${NETWORK}/topology.json \
+        --database-path /db \
+        --port 3000 \
+        --host-addr 0.0.0.0 \
+        --socket-path /ipc/node.socket &
+fi
+wait
