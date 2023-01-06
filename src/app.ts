c7a37d5fe13e55138e2b5f9f57496ff6d7c3ad71
guavajellyaaron <aaron@guavajellyco.com>
2023-01-06 15:02:45 -0800

    Add keepAliveTimeout

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import yaml from 'yamljs';
 import { NODE_ENV, PORT, ORIGIN, CREDENTIALS } from './config';
 import { Routes } from './interfaces/routes.interface';
 import errorMiddleware from './middlewares/error.middleware';
-import { HandleStore } from './repositories/memory/HandleStore';
 import OgmiosService from './services/ogmios/ogmios.service';
 import { dynamicallyLoad, writeConsoleLine } from './utils/util';
 import { DynamicLoadType } from './interfaces/util.interface';
@@ -32,12 +31,14 @@ class App {
     }
 
     public listen() {
-        this.app.listen(this.port, () => {
+        const server = this.app.listen(this.port, () => {
             Logger.log(`=========================================`);
             Logger.log(`============ENV: ${this.env} ============`);
             Logger.log(`🚀 App listening on the port ${this.port}`);
             Logger.log(`=========================================`);
         });
+
+        server.keepAliveTimeout = 61 * 1000;
     }
 
     public getServer() {
