9751de6ca7fb04f2968fb28fed7f02e4d7ba9627
Papa Goose <papa.goose@koralabs.io>
2023-02-08 15:47:50 -0800

    Adding network folders to package json

---
+++ setup_env.sh
@@ -0,0 +1,4 @@
+mkdir -p handles/{preview,preprod,mainnet}/{datum,snapshot}
+touch handles/preview/snapshot/handles.json
+touch handles/preprod/snapshot/handles.json
+touch handles/mainnet/snapshot/handles.json
