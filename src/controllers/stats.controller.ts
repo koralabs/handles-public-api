0fabc0d8a624ef5afc9753f2781e2c11920c6fcd
guavajellyaaron <aaron@guavajellyco.com>
2023-03-02 15:03:46 -0800

    Add stats route tests

--- src/controllers/stats.controller.ts
+++ src/controllers/stats.controller.ts
@@ -7,11 +7,7 @@ class StatsController {
     public index = async (req: Request<RequestWithRegistry>, res: Response, next: NextFunction): Promise<void> => {
         try {
             const handleRepo: IHandlesRepository = new req.params.registry.handlesRepo();
-
-            res.status(handleRepo.getIsCaughtUp() ? 200 : 202).json({
-                total_handles: HandleStore.count(),
-                total_holders: HandleStore.holderAddressIndex.size
-            });
+            res.status(handleRepo.getIsCaughtUp() ? 200 : 202).json(handleRepo.getTotalHandlesStats());
         } catch (error) {
             next(error);
         }
