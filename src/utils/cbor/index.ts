a2cb0c0351f48c85f2e541a87901f1504e5c1a86
Papa Goose <papa.goose@koralabs.io>
2023-10-11 22:11:18 -0700

    Fixing bug with numeric keys

--- src/utils/cbor/index.ts
+++ src/utils/cbor/index.ts
@@ -101,8 +101,11 @@ export const encodeJsonToDatum = async (json: any, numericKeys = false) => {
 const parseSchema = (key: any, schema: any, defaultKeyType: KeyType, i: number) => {
     let schemaValue;
 
-    let mapKey = Buffer.from(`${key}`).toString('utf8');
-    const hexKey = `0x${Buffer.from(`${key}`).toString('hex')}`;
+    if (Number.isInteger(key)) {
+        key = `${key}`
+    }
+    let mapKey = Buffer.from(key).toString('utf8');
+    const hexKey = `0x${Buffer.from(key).toString('hex')}`;
 
     // check schema to see if it matches
 
