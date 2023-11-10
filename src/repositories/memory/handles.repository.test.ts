30a726a1361b97ef59114ecc3181652bdd1584f5
Papa Goose <papa.goose@koralabs.io>
2023-11-09 18:44:57 -0800

    Fixing tests

--- src/repositories/memory/handles.repository.test.ts
+++ src/repositories/memory/handles.repository.test.ts
@@ -308,37 +308,3 @@ describe('MemoryHandlesRepository Tests', () => {
         });
     });
 });
-
-describe('holder index integrity', () => {
-    beforeAll(async () => {
-        HandleStore.eraseStorage();
-    });
-
-    it('holder index should be accurate', async () => {
-        console.log('ZZZZZZZZ STORE', HandleStore.getHandles().length);
-
-        await createRandomHandles(1000, true);
-        await performRandomHandleUpdates(1000, 1001);
-        const testHolderIndex = new Map<string, HolderAddressIndex>();
-        const handles = HandleStore.getHandles().sort((a, b) => a.updated_slot_number - b.updated_slot_number);
-        for (let i = 0; i < handles.length; i++) {
-            const handle = handles[i];
-            const holder = testHolderIndex.get(handle.holder);
-            if (!holder) {
-                const set = new Set<string>();
-                set.add(handle.name);
-                testHolderIndex.set(handle.holder, {
-                    defaultHandle: handle.default_in_wallet,
-                    handles: set,
-                    knownOwnerName: '',
-                    manuallySet: false,
-                    type: 'wallet'
-                });
-            } else {
-                holder.defaultHandle = handle.default_in_wallet;
-                holder.handles.add(handle.name);
-            }
-        }
-        expect(HandleStore.holderAddressIndex).toEqual(testHolderIndex);
-    });
-});
