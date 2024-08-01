8384357581ca8f908effbf3921e3baecf102d079
papag00se <jlanderson@gmail.com>
2024-08-01 13:34:58 -0700

    Adding all mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export NETWORK=${NETWORK:-mainnet}
 export OGMIOS_HOST=${OGMIOS_HOST:-'http://0.0.0.0:1337'}
 export DISABLE_HANDLES_SNAPSHOT=${DISABLE_HANDLES_SNAPSHOT:-false}
 DISABLE_NODE_SNAPSHOT=${DISABLE_NODE_SNAPSHOT:-false}
-MODE=${MODE:-both}
+MODE=${MODE:-all}
 NODE_DB=${NODE_DB:-'/db'}
 SOCKET_PATH=${SOCKET_PATH:-'/ipc/node.socket'}
 
@@ -38,7 +38,7 @@ if [[ "${MODE}" == "ogmios" || "${MODE}" == "both" ]]; then
     sed -i 's https://api.handle.me http://localhost:3141 ' /app/swagger.yml
 fi
 
-if [[ "${MODE}" == "ogmios" || "${MODE}" == "both" || "${MODE}" == "api-only" ]]; then
+if [[ "${MODE}" == "ogmios" || "${MODE}" == "all" || "${MODE}" == "api-only" ]]; then
     sleep 5
     NODE_ENV=${NODE_ENV:-production} NETWORK=${NETWORK} OGMIOS_HOST=${OGMIOS_HOST} DISABLE_HANDLES_SNAPSHOT=${DISABLE_HANDLES_SNAPSHOT:-false} npm run start:forever
 fi
