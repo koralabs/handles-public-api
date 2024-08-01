516e87c9bfbed2f6fa4f8a299495d3aefb209602
papag00se <jlanderson@gmail.com>
2024-08-01 15:59:55 -0700

    Adding `start:local` and `dev:local`

---
+++ start_local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run \
+    -v ~/shared-docker/preview/ipc:/ipc \
+    -v ~/shared-docker/preview/db:/db \
+    -p 1337:1337 \
+    -e MODE=both \
+    -e NETWORK=preview \
+    koralabs/handles-api:local
