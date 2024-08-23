01b272d82a4b7d30f607244b4f71a8e0a04465f2
papag00se <jlanderson@gmail.com>
2024-08-23 13:17:30 -0700

    Losing the ./src folder

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+const config = {
+  "preset": "ts-jest/presets/default-esm",
+  "testEnvironment": "node",
+  "transform": {
+      "^.+\\.tsx?$": "ts-jest",
+    }
+  }
+  export default config
+// export const setupFiles = ["./setupJest.js"];
