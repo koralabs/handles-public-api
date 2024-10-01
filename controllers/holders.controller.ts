7a77d85e425838453149f4392c1654df57d6e3bc
papag00se <jlanderson@gmail.com>
2024-09-30 21:31:07 -0700

    Fixing tests

--- controllers/holders.controller.ts
+++ controllers/holders.controller.ts
@@ -1,6 +1,5 @@
+import { HolderPaginationModel, IGetAllHoldersQueryParams, IGetHolderAddressDetailsRequest, IHandlesRepository } from '@koralabs/kora-labs-common';
 import { NextFunction, Request, Response } from 'express';
-import { IGetAllHoldersQueryParams, IGetHolderAddressDetailsRequest } from '../interfaces/request.interface';
-import { HolderPaginationModel, IHandlesRepository } from '@koralabs/kora-labs-common';
 import { IRegistry } from '../interfaces/registry.interface';
 
 class HoldersController {
