380985676d62c25eacb9b2743ee0d9473877021d
papag00se <jlanderson@gmail.com>
2025-01-16 10:15:02 -0800

    Adding apistore.json to local setup

--- setup_env.sh
+++ setup_env.sh
@@ -5,5 +5,9 @@ mkdir -p block_processors
 touch handles/preview/snapshot/handles.json
 touch handles/preprod/snapshot/handles.json
 touch handles/mainnet/snapshot/handles.json
+touch handles/preview/snapshot/apistore.json
+touch handles/preprod/snapshot/apistore.json
+touch handles/mainnet/snapshot/apistore.json
 touch handles/snapshot/handles.json
+touch handles/snapshot/apistore.json
 cp docs/swagger.yml swagger.yml 2>/dev/null || :
