0eea2812aabab4a7702851e59a2c7405b41a1174
papag00se <jlanderson@gmail.com>
2025-03-06 08:54:17 -0800

    Bumping Ogmios patch version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 set -eu
 
 CARDANO_NODE_VER=${CARDANO_NODE_VER:-10.1.4}
-OGMIOS_VER=${OGMIOS_VER:-6.11.0}
+OGMIOS_VER=${OGMIOS_VER:-6.11.2}
 apt install -y && apt update -y && apt install -y git curl socat jq unzip tini lz4 zstd
 curl -fsSL https://github.com/IntersectMBO/cardano-node/releases/download/${CARDANO_NODE_VER}/cardano-node-${CARDANO_NODE_VER}-linux.tar.gz | tar -xz
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
