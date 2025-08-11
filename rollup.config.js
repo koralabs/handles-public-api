4273c13c07848e0a89c9f59b729a2dbfc39a965c
papag00se <jlanderson@gmail.com>
2025-08-10 21:29:11 -0700

    removing unused deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import multiInput from 'rollup-plugin-multi-input';
-import nodeExternals from 'rollup-plugin-node-externals';
 import typescript from 'rollup-plugin-typescript2';
 
 export default (async () => ({
