8a4b96c6f172c9ecc1d3e727279a9876be6c70b2
papag00se <jlanderson@gmail.com>
2025-08-17 13:45:41 -0700

    multiple rollups

---
+++ rollup.ec2.config.js
@@ -0,0 +1,29 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import multiInput from 'rollup-plugin-multi-input';
+import typescript from 'rollup-plugin-typescript2';
+
+export default (async () => ({
+    input: [
+        'express.ts',
+        'block_processors/*.processor.ts',
+        'ioc/*.registry.ts',
+        'middlewares/*.middleware.ts',
+        'routes/*.route.ts',
+        'workers/*.js'
+    ],
+    plugins: [
+        typescript(),
+        commonjs({ignoreDynamicRequires: true}),
+        nodeResolve(),
+        json(),
+        multiInput()
+    ],
+    output: {
+        dir: 'dist',
+        format: 'es'
+    },
+    external: [ 'aws-sdk', 'forever', 'swagger-ui-express', '@valkey/valkey-glide' ]
+}))();
+
