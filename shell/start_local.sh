644b76ab5ffd7bf2059ec2b5eaf66a1acc836e69
papag00se <jlanderson@gmail.com>
2025-11-20 14:40:44 -0800

    os-release

--- shell/start_local.sh
+++ shell/start_local.sh
@@ -46,13 +46,13 @@ done
 
 if ! pgrep -x "valkey-server" > /dev/null
 then
-    if [ "$(printf '%s\n' 24.04 "$(lsb_release -rs)" | sort -V | head -n1)" = "24.04" ]
+    if [ grep -q "ID=amzn" /etc/os-release ] || [ "$(printf '%s\n' 24.04 "$(lsb_release -rs)" | sort -V | head -n1)" = "24.04" ]
     then
         echo "Starting Valkey - connecting to 6379"
         sudo apt install valkey
         sudo systemctl enable valkey-server
     else
-        echo "You need to update your version of Ubuntu to at least 24.04 to install and run Valkey"
+        echo "You need to update your OS to Ubuntu 24.04 or Amazon Linujx to install and run Valkey"
     fi
 fi
 
